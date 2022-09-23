d08486a3fe403a9a98972e02321f979d36528bbf
Salg-0 <antoniosalgueiro.2.0.0.0.a@gmail.com>
2022-09-23 13:18:30 +0100

    fix: minor dependency fix

--- src/Pages/Tasks/Tasks.tsx
+++ src/Pages/Tasks/Tasks.tsx
@@ -109,18 +109,19 @@ const Tasks = (props: any) => {
       // Update the list of existing boards in the department
       updateExistingBoards(departmentBoard, setExistingBoards);
     }
+
     return () => {
       off(ref(db, `private/${departmentBoard}/b/${currBoard}`));
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [departmentBoard, currBoard, openColId, openTaskId, usersMetadata]);
+  }, [departmentBoard, currBoard, openColId, openTaskId]);
 
   useEffect(() => {
     if (department) {
       // Get users metadata and set the state of user options
       setUserAssignmentOptions(setUserOptions, usersMetadata, department);
     }
-  }, [department]);
+  }, [department, usersMetadata]);
 
   return (
     <div className="app-main__outer">
